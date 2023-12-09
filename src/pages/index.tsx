b7e58c2e6bcf9fe0e3be965cb80395105201a7ab
Shlok D. Mange <shlok_mange@protonmail.com>
2023-12-09 14:49:07 +0530

    safekit init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,42 @@
 import { Box } from '@chakra-ui/react'
 import { NextPage } from 'next'
 
+import {
+	SafeAuthPack,
+	SafeAuthConfig,
+	SafeAuthInitOptions,
+  } from '@safe-global/auth-kit'
+
+	  
 const Home: NextPage = () => {
+		  
+	const createNewSafe = async (rpcUrl: string) => {
+		
+		const safeAuthInitOptions: SafeAuthInitOptions = {
+			enableLogging: true,
+			showWidgetButton: false,
+			chainConfig: {
+			  chainId: '0x1',
+			  rpcTarget: `${rpcUrl}`
+			},
+		}
+
+		const safeAuthConfig: SafeAuthConfig = {
+			txServiceUrl: 'https://safe-transaction-mainnet.safe.global'
+		}
+
+		const safeAuthPack = new SafeAuthPack(safeAuthConfig)
+		
+		await safeAuthPack.init(safeAuthInitOptions)
+
+		const authKitSignData = await safeAuthPack.signIn()
+
+		console.log(authKitSignData)
+	}
+
 	return (
 		<Box>
-			
+
 		</Box>
 	)
 }
