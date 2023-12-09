0a99a7d8d6797d1be2c8adb341bed2b8af00be31
Shlok D. Mange <shlok_mange@protonmail.com>
2023-12-09 21:12:44 +0530

    ui updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,17 +3,45 @@ import { ChakraProvider } from '@chakra-ui/react'
 import '@fontsource/major-mono-display'
 import { WagmiConfig, createConfig } from 'wagmi'
 import { ConnectKitProvider, getDefaultConfig } from 'connectkit'
-import { polygonMumbai, arbitrumSepolia, baseGoerli, mainnet, polygonZkEvmTestnet, goerli } from 'wagmi/chains'
+import { polygonMumbai, 
+  arbitrumSepolia, 
+  baseGoerli, 
+  mainnet, 
+  polygonZkEvmTestnet, 
+  goerli,
+  scrollTestnet,
+  baseSepolia,
+  mantleTestnet,
+  celoAlfajores,
+  zetachainAthensTestnet,
+  lineaTestnet,
+  xdcTestnet 
+} from 'wagmi/chains'
 import { FC } from 'react'
 import theme from '@/theme.json'
 
-const chains = [goerli, polygonMumbai, arbitrumSepolia, baseGoerli, mainnet, polygonZkEvmTestnet]
+const chains = [
+  goerli, 
+  polygonMumbai, 
+  arbitrumSepolia, 
+  baseGoerli, 
+  mainnet, 
+  polygonZkEvmTestnet,
+  scrollTestnet,
+  baseSepolia,
+  mantleTestnet,
+  celoAlfajores,
+  zetachainAthensTestnet,
+  lineaTestnet,
+  xdcTestnet
+]
 
 const config = createConfig(
 	getDefaultConfig({
 		alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_ID as string,
+    infuraId: process.env.NEXT_PUBLIC_INFURA_ID as string,
 		walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
-		appName: 'Safe Intents',
+		appName: 'Valmor Protocol',
 		chains
 	})
 )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,22 @@
-'use client'
-
-import { Box, Button, Heading, Stack, Text, VStack } from '@chakra-ui/react'
+import { Box, Button, HStack, Heading, Input, Stack, Text, VStack } from '@chakra-ui/react'
 import { NextPage } from 'next'
-
+import { Intents } from '@bytekode/intents'
+import { useAccount } from 'wagmi'
+import { useState } from 'react'
 import {
 	SafeAuthPack,
 	SafeAuthConfig,
 	SafeAuthInitOptions,
   } from '@safe-global/auth-kit'
 
+import { ConnectKitButton } from 'connectkit'
+
 	  
 const Home: NextPage = () => {
-		  
+	
+	const { address, isConnected, connector } = useAccount()
+	const [ command, setCommand] = useState('')
+
 	// const createNewSafe = async (rpcUrl: string) => {
 		
 	// 	const safeAuthInitOptions: SafeAuthInitOptions = {
@@ -39,6 +44,30 @@ const Home: NextPage = () => {
 	{/* <Button onClick={() => createNewSafe(`https://goerli.rpc.thirdweb.com`)}>Login</Button> */}
 	
 	const heroText = `Interchain 1-click defi automation `
+	
+	const executeIntents = async () => {
+		const url = 'http://localhost:8080/payments';
+		const body = JSON.stringify({
+			chainId: (await connector!.getChainId()).toString(),
+			command: command,
+			address: address,
+		});
+
+		try {
+			const response = await fetch(url, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: body,
+			});
+
+			const txObjects = response.body
+		} catch (error) {
+			// Handle network error
+		}
+	};
+	
 	return (
 
 		<Stack
@@ -50,27 +79,51 @@ const Home: NextPage = () => {
 			height="100vh"
 			spacing={8}
 		>
-			<Stack px={[4,8,12]} p={[4,8,12]}>
+			<HStack px={[4,8,12]} p={[4,8,12]} justify={'space-between'}>
 				<Heading fontFamily={'Major Mono Display'} color={'gray.400'}>
-					valmor
+					Valmor
 				</Heading>
-			</Stack>
-			<VStack fontFamily={'monospace'}>
-				<Text fontSize={'3xl'} color={'gray.300'} fontFamily={'Major Mono Display'}>{heroText}</Text>
-				<Button
-					mt={4}
-					style={{
-						borderRadius: 0,
-						backgroundColor: 'rgba(255, 255, 255, 0.1)',
-						fontFamily: 'monospace'
-					}}
-					color={'gray.300'}
-					fontSize={'xl'}
-					fontWeight={'regular'}
-				>
-					Get Started
-				</Button>
-			</VStack>
+				{
+					isConnected && (<Text color={'gray.400'}>
+						{address}
+					</Text>)
+				}
+			</HStack>
+			{
+				!isConnected ? 
+				(
+					<VStack fontFamily={'monospace'}>
+						<Text fontSize={'3xl'} color={'gray.300'} fontFamily={'Major Mono Display'}>{heroText}</Text>
+						{/* <Button
+							mt={4}
+							style={{
+								borderRadius: 0,
+								backgroundColor: 'rgba(255, 255, 255, 0.1)',
+								fontFamily: 'monospace'
+							}}
+							color={'gray.300'}
+							fontSize={'xl'}
+							fontWeight={'regular'}
+						>
+							Get Started
+						</Button> */}
+
+						<Stack mt={4}>
+							<ConnectKitButton />
+						</Stack>
+					</VStack>
+				) :
+				(
+					<VStack>
+						<Input 
+							value={command} 
+							onChange={(e) => setCommand(e.target.value)}
+							w={'50%'}
+						/>
+						<Button onClick={executeIntents}>execute</Button>
+					</VStack>
+				)
+			}
 		</Stack>
 	)
 }
