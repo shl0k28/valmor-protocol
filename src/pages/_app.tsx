5fc8b47bc460dad31638577c2c4848f96dbd2bef
Shlok D. Mange <shlok_mange@protonmail.com>
2023-12-09 17:54:16 +0530

    connectkit init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,33 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import '@fontsource/major-mono-display'
+import { WagmiConfig, createConfig } from 'wagmi'
+import { ConnectKitProvider, getDefaultConfig } from 'connectkit'
+import { polygonMumbai, arbitrumSepolia, baseGoerli, mainnet, polygonZkEvmTestnet, goerli } from 'wagmi/chains'
+import { FC } from 'react'
+import theme from '@/theme.json'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+const chains = [goerli, polygonMumbai, arbitrumSepolia, baseGoerli, mainnet, polygonZkEvmTestnet]
+
+const config = createConfig(
+	getDefaultConfig({
+		alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_ID as string,
+		walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
+		appName: 'Safe Intents',
+		chains
+	})
+)
+
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+	return(
+		<WagmiConfig config={config}>
+			<ConnectKitProvider customTheme={theme}>
+				<ChakraProvider>
+					<Component {...pageProps} />
+				</ChakraProvider>
+			</ConnectKitProvider>
+		</WagmiConfig>
   )
 }
+
+export default App
